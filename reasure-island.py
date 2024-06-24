c8d410c56a37711b7e026afe9ded39a76407f577
ABDELILAH AIT AHMED OUHAMOU <96702843+tyub101@users.noreply.github.com>
2024-06-24 16:15:33 +0100

    Update reasure-island.py

--- reasure-island.py
+++ reasure-island.py
@@ -1,5 +1,3 @@
-import re
-
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -24,35 +22,23 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-# Write your code below this line 👇
-import re
 
-# Define the regex pattern for any word
-pattern = re.compile(r'^\w+$')
+#Write your code below this line 👇
 
-direction = input("left or right? ")
-
-# Check if direction is "right" or matches the pattern for any word
-if direction.lower() == "right":
-    print("Fall into a hole.\nGame Over.")
-elif direction.lower() == "left":
-    swim_or_wait=input("swim or wait")
-    if swim_or_wait.lower()=="swim":
-              print("Attacked by trout.\nGame Over.")
-    elif swim_or_wait.lower()=="wait":
-        door=input("Which door?")
-        if door.lower()=="blue":
-                  print("Eaten by beasts.\nGame Over.")
-        elif door.lower()=="red" :
-                  print("Burned by fire.\nGame Over.")
-        elif door.lower()=="yellow" :
-                  print("You Win!")
-        else: 
-                  print("Game Over.")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
     else:
-          print("Attacked by trout.\nGame Over.")
-
-else: 
-    print("Fall into a hole.\nGame Over.")
-  
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
